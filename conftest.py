22a8ee564d014c13f3206adc3c2e05c1548b25c7
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-06-08 22:49:27 -0400

    Apply suggestions from code review
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -1,16 +1,18 @@
 """Pytest configuration file."""
 
+from typing import Final
+
 from sybil import Sybil
 from sybil.parsers.doctest import DocTestParser
 
-readme_tester = Sybil(
+readme_tester: Final = Sybil(
     parsers=[DocTestParser()],
     pattern="README.md",
 )
 
-python_file_tester = Sybil(
+python_file_tester: Final = Sybil(
     parsers=[DocTestParser()],
     pattern="src/**/*.py",
 )
 
-pytest_collect_file = (readme_tester + python_file_tester).pytest()
+pytest_collect_file: Final = (readme_tester + python_file_tester).pytest()
