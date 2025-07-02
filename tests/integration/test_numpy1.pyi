9c89224783766d38bd3fcdd0dee6645417c0b251
jorenham <jhammudoglu@gmail.com>
2025-07-02 20:21:57 +0200

    🩹 don't attempt to directly use `np.array_api.Array`

--- tests/integration/test_numpy1.pyi
+++ tests/integration/test_numpy1.pyi
@@ -1,5 +1,6 @@
 from typing import Any
 
+# requires numpy < 2
 import numpy.array_api as np
 
 import array_api_typing as xpt
@@ -7,5 +8,5 @@ import array_api_typing as xpt
 ###
 # Ensure that `np.ndarray` instances are assignable to `xpt.HasArrayNamespace`.
 
-arr: np.Array
+arr = np.eye(2)
 arr_namespace: xpt.HasArrayNamespace[Any] = arr
