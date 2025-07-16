876b4a59284bff62ca511b7969753330d211729e
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-07-16 18:12:37 +0000

    ✨: refactor test files to improve clarity and organization of NDArray definitions

--- tests/integration/test_numpy1.pyi
+++ tests/integration/test_numpy1.pyi
@@ -5,8 +5,16 @@ import numpy.array_api as np
 
 import array_api_typing as xpt
 
+# Define an NDArray against which we can test the protocols
+arr = np.eye(2)
+
 ###
 # Ensure that `np.ndarray` instances are assignable to `xpt.HasArrayNamespace`.
 
-arr = np.eye(2)
 arr_namespace: xpt.HasArrayNamespace[Any] = arr
+
+###
+# Ensure that `np.ndarray` instances are assignable to `xpt.Array`.
+
+arr_array: xpt.Array[Any, Any] = arr
+arr_floatarray: xpt.Array[float, Any] = arr
--- tests/integration/test_numpy2.pyi
+++ tests/integration/test_numpy2.pyi
@@ -4,8 +4,17 @@ import numpy.typing as npt
 
 import array_api_typing as xpt
 
+# Define an NDArray against which we can test the protocols
+arr: npt.NDArray[Any]
+
 ###
 # Ensure that `np.ndarray` instances are assignable to `xpt.HasArrayNamespace`.
 
-arr: npt.NDArray[Any]
 arr_namespace: xpt.HasArrayNamespace[Any] = arr
+
+###
+# Ensure that `np.ndarray` instances are assignable to `xpt.Array`.
+
+arr_array: xpt.Array[Any, Any] = arr
+arr_floatarray: xpt.Array[float, Any] = arr
+arr_boolarray: xpt.Array[bool, Any] = arr
